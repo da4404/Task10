ef409940cea2821b5221fd1be3470b1e34f7e963
בר אופק <u9054555@bsmch.net>
2025-04-19 22:30:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/task10/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.task10;
+
+import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
