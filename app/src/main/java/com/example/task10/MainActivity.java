1c837c76220bbb2c75a4422f111956b57c62a092
בר אופק <u9054555@bsmch.net>
2025-04-19 23:19:29 +0300

    עובד!!!!!

--- app/src/main/java/com/example/task10/MainActivity.java
+++ app/src/main/java/com/example/task10/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.task10;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.ToggleButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-public class MainActivity extends AppCompatActivity {
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+public class MainActivity extends AppCompatActivity {
+    ToggleButton tb;
+    ConstraintLayout screen;
+    Switch swDN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tb = (ToggleButton)findViewById(R.id.tb);
+        swDN = (Switch)findViewById(R.id.swDN);
+        screen = (ConstraintLayout)findViewById(R.id.main);
+
+    }
+
+    public void check_image(View view)
+    {
+        if (!swDN.isChecked() && !tb.isChecked())
+        {
+            screen.setBackgroundResource(R.drawable.image1);
+        }
+        else if (!swDN.isChecked() && tb.isChecked())
+        {
+            screen.setBackgroundResource(R.drawable.image2);
+        }
+        else if (swDN.isChecked() && !tb.isChecked())
+        {
+            screen.setBackgroundResource(R.drawable.image3);
+        }
+        else
+        {
+            screen.setBackgroundResource(R.drawable.image4);
+
+        }
 
     }
 }
